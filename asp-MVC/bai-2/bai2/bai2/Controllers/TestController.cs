9d4ccfd098b1dc63297c79fc29047adb21e4778f
phu13 <87376317+phu13@users.noreply.github.com>
2021-12-21 18:11:18 +0700

    lam bai 1

---
+++ asp-MVC/bai-2/bai2/bai2/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bai2.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            ViewBag.WelcomeString = "Hello From ViewBag";
+            var message = new Models.MessageModel();
+            message.welcome = "Welcome from model";
+            return View(message);
+        }
+    }
+}

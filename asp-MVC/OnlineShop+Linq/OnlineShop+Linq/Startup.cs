56f866f23ef5b132b9cbf464bdb253bbd67c4db5
phu13 <87376317+phu13@users.noreply.github.com>
2021-12-28 10:08:42 +0700

    bai 10

---
+++ asp-MVC/OnlineShop+Linq/OnlineShop+Linq/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineShop_Linq.Startup))]
+namespace OnlineShop_Linq
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

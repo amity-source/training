c3f3c02f72b42e1042c2dc4a4442fc5e5dbf1444
phu13 <87376317+phu13@users.noreply.github.com>
2022-01-13 14:04:52 +0700

    bai-27
    Cách sử dụng file Web.config | How to use Web.Config file | TEDU

--- asp-MVC/OnlineShop+Linq/OnlineShop+Linq/Areas/Admin/Controllers/ContentController.cs
+++ asp-MVC/OnlineShop+Linq/OnlineShop+Linq/Areas/Admin/Controllers/ContentController.cs
@@ -3,7 +3,7 @@ using Model.EF;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Web;   
 using System.Web.Mvc;
 
 namespace OnlineShop_Linq.Areas.Admin.Controllers

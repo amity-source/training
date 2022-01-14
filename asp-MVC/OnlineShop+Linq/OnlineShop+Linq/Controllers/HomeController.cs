c28d8b2c05e92a3c956316397c27ac0dcd3b595b
phu13 <87376317+phu13@users.noreply.github.com>
2022-01-14 17:44:19 +0700

    bai-30
    30: Dựng giao diện cho client (trang bên ngoài) | Create UI for client site

--- asp-MVC/OnlineShop+Linq/OnlineShop+Linq/Controllers/HomeController.cs
+++ asp-MVC/OnlineShop+Linq/OnlineShop+Linq/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using OnlineShop_Linq.Areas.Admin.Controllers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,24 +6,11 @@ using System.Web.Mvc;
 
 namespace OnlineShop_Linq.Controllers
 {
-    public class HomeController : BaseController
+    public class HomeController : Controller
     {
+        // GET: Home
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
